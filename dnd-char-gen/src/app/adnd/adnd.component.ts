bb72062d92b42b30b6d5d67dcc47d2ff2de7b0a3
Gabe Haggard <gabehagg@gmail.com>
2018-11-18 08:50:37 -0800

    added brick background, and ADND is mostly functional

--- dnd-char-gen/src/app/adnd/adnd.component.ts
+++ dnd-char-gen/src/app/adnd/adnd.component.ts
@@ -9,7 +9,38 @@ export class ADNDComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  strength :number = 0
+  dexeterity :number = 0
+  constitution :number = 0
+  intellegence :number = 0
+  wisdom :number = 0
+  charisma :number = 0
+
+  dwarfAdj(){
+    this.constitution  += 1
+    this.charisma -= 1
   }
+  elfAdj(){
+    this.dexeterity  += 1
+    this.constitution -= 1
+  }
+  gnomeAdj(){
+    this.intellegence  += 1
+    this.wisdom -= 1
+  }
+  halfAdj(){
+    this.dexeterity  += 1
+    this.strength -= 1
+  }
+  
+  ngOnInit() {
+    this.strength = Math.floor(Math.random() * 18) + 3
+    this.dexeterity = Math.floor(Math.random() * 18) + 3
+    this.constitution = Math.floor(Math.random() * 18) + 3
+    this.intellegence = Math.floor(Math.random() * 18) + 3
+    this.wisdom = Math.floor(Math.random() * 18) + 3
+    this.charisma = Math.floor(Math.random() * 18) + 3
+  }
+
 
 }
